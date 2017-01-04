bf05a08b76ced42de2dbe45de370ebf249b5d61f
Michael Joyce <ubermichael@gmail.com>
2017-01-04 15:15:37 -0800

    start the blogging.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new AppUserBundle\AppUserBundle(),
             new FeedbackBundle\FeedbackBundle(),
+            new Nines\BlogBundle\NinesBlogBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
