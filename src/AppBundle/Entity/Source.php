4f16f2cfeebd211639b52f2b7e15fc253e2b6576
Michael Joyce <ubermichael@gmail.com>
2017-03-20 11:08:50 -0700

    Updates from the database migration.

--- src/AppBundle/Entity/Source.php
+++ src/AppBundle/Entity/Source.php
@@ -28,7 +28,23 @@ class Source
      */
     private $name;
 
+    /**
+     * @var boolean
+     * @ORM\Column(name="local", type="boolean", nullable=false, options={"default" : 0})
+     */
+    private $local;
 
+    /**
+     * @var string
+     * @ORM\Column(name="url", type="string", length=60, nullable=true)
+     */
+    private $url;
+    
+    /**
+     * @var string
+     * @ORM\Column(name="sourcetable", type="string", length=60, nullable=true)
+     */
+    private $sourceTable;
 
     /**
      * Get id
@@ -62,5 +78,77 @@ class Source
     public function getName()
     {
         return $this->name;
+    }
+
+    /**
+     * Set local
+     *
+     * @param boolean $local
+     *
+     * @return Source
+     */
+    public function setLocal($local)
+    {
+        $this->local = $local;
+
+        return $this;
+    }
+
+    /**
+     * Get local
+     *
+     * @return boolean
+     */
+    public function getLocal()
+    {
+        return $this->local;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     *
+     * @return Source
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Set sourceTable
+     *
+     * @param string $sourceTable
+     *
+     * @return Source
+     */
+    public function setSourceTable($sourceTable)
+    {
+        $this->sourceTable = $sourceTable;
+
+        return $this;
+    }
+
+    /**
+     * Get sourceTable
+     *
+     * @return string
+     */
+    public function getSourceTable()
+    {
+        return $this->sourceTable;
     }
 }
