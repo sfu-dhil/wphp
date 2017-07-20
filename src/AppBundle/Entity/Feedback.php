aeb5a20abb4d27baef4c370b9e59524a217c9fdb
Erik Hanson <ewhanson@sfu.ca>
2017-07-20 16:33:35 -0700

    Add feedback submission feature

---
+++ src/AppBundle/Entity/Feedback.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use DateTime;
+
+/**
+ * Feedback
+ *
+ * @ORM\Table(name="feedback")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\FeedbackRepository")
+ * @ORM\HasLifeCycleCallbacks
+ */
+class Feedback
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     * @Assert\Email(
+     *     message = "The email '{{ value }}' is not a valid email.",
+     *     checkMX = true
+     * )
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text")
+     */
+    private $content;
+  
+    /**
+     * The DateTime the entity was created (persisted really).
+     * 
+     * @var DateTime
+     * @ORM\Column(name="created", type="datetime", options={"default": 0})
+     */
+    private $created;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Feedback
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Feedback
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return Feedback
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+  
+    /**
+     * Sets the created timestamps.
+     * 
+     * @ORM\PrePersist()
+     * 
+     * @return void
+     */
+    public function prePersist() {
+      if(! isset($this->created)) {
+        $this->created = new DateTime();
+      }
+    }
+  
+    /**
+     * Does nothing.
+     * 
+     */
+    private function setCreated() {
+      // Does nothing
+    }
+  
+    /**
+     * Get the created timestamp
+     *
+     */
+    public function getCreated() {
+      return $this->created;
+    }
+}
+
