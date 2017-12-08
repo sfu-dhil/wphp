cac375a69ebfd0a6b5fb05affe6a36e5b9b14524
Michael Joyce <ubermichael@gmail.com>
2017-12-08 13:13:47 -0800

    start testing the repositories where possible.

--- src/AppBundle/Tests/Repository/FirmRepositoryTest.php
+++ src/AppBundle/Tests/Repository/FirmRepositoryTest.php
@@ -2,9 +2,9 @@
 
 namespace AppBundle\Tests\Repository;
 
-use AppBundle\DataFixtures\ORM\LoadFirm;
 use AppBundle\Entity\Firm;
-use Nines\UtilBundle\Tests\Util\BaseTestCase;
+use AppBundle\Tests\DataFixtures\ORM\LoadFirm;
+use AppBundle\Tests\Util\BaseTestCase;
 
 class FirmRepositoryTest extends BaseTestCase {
     
@@ -24,7 +24,7 @@ class FirmRepositoryTest extends BaseTestCase {
     
     public function testNextNull() {
         $repo = $this->em->getRepository(Firm::class);
-        $firm = $this->getReference('firm.3');
+        $firm = $this->getReference('firm.2');
         $next = $repo->next($firm);
         $this->assertNull($next);
     }
@@ -39,40 +39,16 @@ class FirmRepositoryTest extends BaseTestCase {
     
     public function testPreviousNull() {
         $repo = $this->em->getRepository(Firm::class);
-        $firm = $this->getReference('firm.0');
+        $firm = $this->getReference('firm.1');
         $previous = $repo->previous($firm);
         $this->assertNull($previous);
     }
     
-    /**
-     * @dataProvider buildSearchQueryData
-     */
-    public function testBuildSearchQuery($data, $expected) {
+    public function testRandom() {
+        $this->markTestSkipped('Cannot test method with SQLite.');        
         $repo = $this->em->getRepository(Firm::class);
-        $query = $repo->buildSearchQuery($data);
-        $this->assertStringEndsWith($expected, $query->getSql());
+        $firm = $repo->random(1);
+        $this->assertNotNull($firm);
+        $this->assertEquals(1, $firm->count());
     }
-    
-    public function buildSearchQueryData() {
-        return [
-            [[], 'FROM firm f0_'],
-            
-            [['address' => 'great'], 'FROM firm f0_ WHERE MATCH (f0_.street_address) AGAINST (? IN BOOLEAN MODE) > 0'],
-            [['name' => 'great'], 'FROM firm f0_ WHERE MATCH (f0_.name) AGAINST (? IN BOOLEAN MODE) > 0'],
-            [['city' => 'jordan'], 'FROM firm f0_ INNER JOIN geonames g1_ ON f0_.city = g1_.geonameid WHERE MATCH (g1_.alternatenames, g1_.name) AGAINST (? IN BOOLEAN MODE) > 0'],
-            
-            [['start' => '1900'], 'FROM firm f0_ WHERE YEAR(f0_.start_date) = ?'],
-            [['start' => '1910-1900'], 'FROM firm f0_ WHERE ? <= YEAR(f0_.start_date) AND YEAR(f0_.start_date) <= ?'],
-            [['start' => '*-1900'], 'FROM firm f0_ WHERE ? <= YEAR(f0_.start_date) AND YEAR(f0_.start_date) <= ?'],
-            [['start' => '1900-*'], 'FROM firm f0_ WHERE ? <= YEAR(f0_.start_date) AND YEAR(f0_.start_date) <= ?'],
-            
-            [['end' => '1900'], 'FROM firm f0_ WHERE YEAR(f0_.end_date) = ?'],
-            [['end' => '1910-1900'], 'FROM firm f0_ WHERE ? <= YEAR(f0_.end_date) AND YEAR(f0_.end_date) <= ?'],
-            [['end' => '*-1900'], 'FROM firm f0_ WHERE ? <= YEAR(f0_.end_date) AND YEAR(f0_.end_date) <= ?'],
-            [['end' => '1900-*'], 'FROM firm f0_ WHERE ? <= YEAR(f0_.end_date) AND YEAR(f0_.end_date) <= ?'],
-            
-            [['address' => 'great', 'name' => 'great'], 'FROM firm f0_ WHERE MATCH (f0_.name) AGAINST (? IN BOOLEAN MODE) > 0 AND MATCH (f0_.street_address) AGAINST (? IN BOOLEAN MODE) > 0'],
-        ];
-    }
-    
 }
