effd7ff9fa139312448f35cdd0a7992029f0ab30
Michael Joyce <ubermichael@gmail.com>
2017-12-08 17:02:59 -0800

    add more tests and fix some bugs.

--- src/AppBundle/Tests/Repository/FirmRepositoryTest.php
+++ src/AppBundle/Tests/Repository/FirmRepositoryTest.php
@@ -47,31 +47,31 @@ class FirmRepositoryTest extends BaseTestCase {
     /**
      * @dataProvider buildSearchQueryData
      */
-    public function testBuildSearchQuery($data, $assertion, $expected) {
+    public function testBuildSearchQuery($data, $expected) {
         $repo = $this->em->getRepository(Firm::class);
         $query = $repo->buildSearchQuery($data);
-        $this->$assertion($expected, $query->getSql());
+        $this->assertStringEndsWith($expected, $query->getSql());
     }
     
     public function buildSearchQueryData() {
         return [
-            [[], 'assertStringEndsWith', 'FROM firm f0_'],
+            [[], 'FROM firm f0_'],
             
-            [['address' => 'great'], 'assertStringEndsWith', 'FROM firm f0_ WHERE MATCH (f0_.street_address) AGAINST (? IN BOOLEAN MODE) > 0'],
-            [['name' => 'great'], 'assertStringEndsWith', 'FROM firm f0_ WHERE MATCH (f0_.name) AGAINST (? IN BOOLEAN MODE) > 0'],
-            [['city' => 'jordan'], 'assertStringEndsWith', 'FROM firm f0_ INNER JOIN geonames g1_ ON f0_.city = g1_.geonameid WHERE MATCH (g1_.alternatenames, g1_.name) AGAINST (? IN BOOLEAN MODE) > 0'],
+            [['address' => 'great'], 'FROM firm f0_ WHERE MATCH (f0_.street_address) AGAINST (? IN BOOLEAN MODE) > 0'],
+            [['name' => 'great'], 'FROM firm f0_ WHERE MATCH (f0_.name) AGAINST (? IN BOOLEAN MODE) > 0'],
+            [['city' => 'jordan'], 'FROM firm f0_ INNER JOIN geonames g1_ ON f0_.city = g1_.geonameid WHERE MATCH (g1_.alternatenames, g1_.name) AGAINST (? IN BOOLEAN MODE) > 0'],
             
-            [['start' => '1900'], 'assertStringEndsWith', 'FROM firm f0_ WHERE YEAR(f0_.start_date) = ?'],
-            [['start' => '1910-1900'], 'assertStringEndsWith', 'FROM firm f0_ WHERE ? <= YEAR(f0_.start_date) AND YEAR(f0_.start_date) <= ?'],
-            [['start' => '*-1900'], 'assertStringEndsWith', 'FROM firm f0_ WHERE ? <= YEAR(f0_.start_date) AND YEAR(f0_.start_date) <= ?'],
-            [['start' => '1900-*'], 'assertStringEndsWith', 'FROM firm f0_ WHERE ? <= YEAR(f0_.start_date) AND YEAR(f0_.start_date) <= ?'],
+            [['start' => '1900'], 'FROM firm f0_ WHERE YEAR(f0_.start_date) = ?'],
+            [['start' => '1910-1900'], 'FROM firm f0_ WHERE ? <= YEAR(f0_.start_date) AND YEAR(f0_.start_date) <= ?'],
+            [['start' => '*-1900'], 'FROM firm f0_ WHERE ? <= YEAR(f0_.start_date) AND YEAR(f0_.start_date) <= ?'],
+            [['start' => '1900-*'], 'FROM firm f0_ WHERE ? <= YEAR(f0_.start_date) AND YEAR(f0_.start_date) <= ?'],
             
-            [['end' => '1900'], 'assertStringEndsWith', 'FROM firm f0_ WHERE YEAR(f0_.end_date) = ?'],
-            [['end' => '1910-1900'], 'assertStringEndsWith', 'FROM firm f0_ WHERE ? <= YEAR(f0_.end_date) AND YEAR(f0_.end_date) <= ?'],
-            [['end' => '*-1900'], 'assertStringEndsWith', 'FROM firm f0_ WHERE ? <= YEAR(f0_.end_date) AND YEAR(f0_.end_date) <= ?'],
-            [['end' => '1900-*'], 'assertStringEndsWith', 'FROM firm f0_ WHERE ? <= YEAR(f0_.end_date) AND YEAR(f0_.end_date) <= ?'],
+            [['end' => '1900'], 'FROM firm f0_ WHERE YEAR(f0_.end_date) = ?'],
+            [['end' => '1910-1900'], 'FROM firm f0_ WHERE ? <= YEAR(f0_.end_date) AND YEAR(f0_.end_date) <= ?'],
+            [['end' => '*-1900'], 'FROM firm f0_ WHERE ? <= YEAR(f0_.end_date) AND YEAR(f0_.end_date) <= ?'],
+            [['end' => '1900-*'], 'FROM firm f0_ WHERE ? <= YEAR(f0_.end_date) AND YEAR(f0_.end_date) <= ?'],
             
-            [['address' => 'great', 'name' => 'great'], 'assertStringEndsWith', 'FROM firm f0_ WHERE MATCH (f0_.name) AGAINST (? IN BOOLEAN MODE) > 0 AND MATCH (f0_.street_address) AGAINST (? IN BOOLEAN MODE) > 0'],
+            [['address' => 'great', 'name' => 'great'], 'FROM firm f0_ WHERE MATCH (f0_.name) AGAINST (? IN BOOLEAN MODE) > 0 AND MATCH (f0_.street_address) AGAINST (? IN BOOLEAN MODE) > 0'],
         ];
     }
     
