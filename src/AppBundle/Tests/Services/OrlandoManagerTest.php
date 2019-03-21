aa95f90bd3ba47227fedbf05ddce8a19594316c3
Michael Joyce <ubermichael@gmail.com>
2019-03-21 06:49:27 -0700

    fix a badly named test.

--- src/AppBundle/Tests/Services/OrlandoManagerTest.php
+++ src/AppBundle/Tests/Services/OrlandoManagerTest.php
@@ -7,7 +7,7 @@ use AppBundle\DataFixtures\ORM\LoadOrlandoBiblio;
 use AppBundle\Services\OrlandoManager;
 use Nines\UtilBundle\Tests\Util\BaseTestCase;
 
-class SourceLinkerTest extends BaseTestCase
+class OrlandoManagerTest extends BaseTestCase
 {
 
     const DATA = 'A_ID = 20384 || STANDARD = Author 2 || ROLE = EDITOR %%% A_ID = 19884 || STANDARD = Other Author 2 || ROLE = AUTHOR';
