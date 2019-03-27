a700359a6def31476d165d1404be4d6e815b2fbb
Michael Joyce <ubermichael@gmail.com>
2019-03-27 12:50:49 -0700

    fix some tests.

--- src/AppBundle/Tests/Services/EstcMarcImporterTest.php
+++ src/AppBundle/Tests/Services/EstcMarcImporterTest.php
@@ -36,145 +36,144 @@ class EstcMarcImporterTest extends BaseTestCase {
         );
     }
 
-//    public function testGetFields() {
-//        $f1 = new EstcMarc();
-//        $f1->setTitleId('abc')->setField('100')->setSubfield('a')->setFieldData('Bond, James');
-//        $f2 = new EstcMarc();
-//        $f2->setTitleId('abc')->setField('100')->setSubfield('b')->setFieldData('Moneypenny, Miss');
-//
-//        $repo = $this->createMock(EstcMarcRepository::class);
-//        $repo->method('findBy')->willReturn(array($f1, $f2));
-//        $this->importer->setEstcRepo($repo);
-//
-//        $data = $this->importer->getFields('foo');
-//        $this->assertCount(2, $data);
-//        $this->assertEquals(array('100a', '100b'), array_keys($data));
-//        $this->assertEquals('Bond, James', $data['100a']->getFieldData());
-//        $this->assertEquals('Moneypenny, Miss', $data['100b']->getFieldData());
-//    }
-//
-//    public function testCheckTitle() {
-//        $repo = $this->createMock(TitleRepository::class);
-//        $repo->method('findAll')->willReturn(array('a', 'b'));
-//        $this->importer->setTitleRepo($repo);
-//
-//        $importer = $this->getContainer()->get(EstcMarcImporter::class);
-//        $this->assertEquals(2, $importer->checkTitle('fooo'));
-//        $this->assertCount(1, $importer->getMessages());
-//    }
-//
-//    /**
-//     * @dataProvider getDatesData
-//     */
-//    public function testGetDates($expectedDob, $expectedDod, $data) {
-//        $f2 = new EstcMarc();
-//        $f2->setField('100')->setSubfield('d')->setFieldData($data);
-//        list($dob, $dod) = $this->importer->getDates(array('100d' => $f2));
-//        $this->assertEquals($expectedDob, $dob);
-//        $this->assertEquals($expectedDod, $dod);
-//    }
-//
-//    public function getDatesData() {
-//        return array(
-//            ['1751', '1801', '1751-1801'],
-//            ['1751', '1801', '1751-1801.'],
-//            ['1751', '1801', '1751-1801,'],
-//            [null, null, null],
-//
-//            // this is actual data. sigh.
-//            ['1698', '1709', 'active 1698-1709.'],
-//            ['1468', '1522', '1468?-1522.'],
-//            [null, '1714', '-1714]'],
-//            [null, '1147', '-1147?.'],
-//            [null, 1716, '-1716.'],
-//            ['1770', '1820', '1770?-1820?.'],
-//            ['1770', null, '1770?-.'],
-//            [null, '1520', '-1520 or 1521.'],
-//            [null, '1783', '-1783 November 17.'],
-//            [null, null, '-approximately 1676.'],
-//            ['1752', '1820', '. 1752-1820.'],
-//            ['1763', null, '.b. 1763.'],
-//            [null, null, '100 B.C.-44 B.C.'],
-//            ['1142', '1165', '1141 or 1142-1165.'],
-//            [null, null, '121-180.'],
-//            ['1388', '', '1388?.-'],
-//            ['1580', '1653', '1580-1653)]'],
-//            ['1590', null, '1590-approximately 1645.'],
-//            ['1603', null, '1602 or 1603-'],
-//            ['1605', null, '1605-'],
-//        );
-//    }
-//
-//    public function testGetPerson() {
-//        $f1 = new EstcMarc();
-//        $f1->setField('100')->setSubfield('a')->setFieldData('LastName 1, FirstName 1.');
-//        $f2 = new EstcMarc();
-//        $f2->setField('100d')->setSubfield('d')->setFieldData('1751-1801.');
-//
-//        $personCount = $this->em->getRepository(Person::class)->count(array());
-//        $this->em->clear();
-//
-//        $person = $this->importer->getPerson(array('100a' => $f1, '100d' => $f2));
-//        $this->assertInstanceOf(Person::class, $person);
-//        $this->assertEquals($personCount, $this->em->getRepository(Person::class)->count(array()));
-//        $this->assertCount(0, $this->importer->getMessages());
-//        $this->assertEquals('LastName 1', $person->getLastName());
-//    }
-//
-//    public function testGetPersonNoDates() {
-//        $f1 = new EstcMarc();
-//        $f1->setField('100')->setSubfield('a')->setFieldData('LastName 1, FirstName 1.');
-//        $personCount = $this->em->getRepository(Person::class)->count(array());
-//        $this->em->clear();
-//
-//        $person = $this->importer->getPerson(array('100a' => $f1));
-//        $this->assertInstanceOf(Person::class, $person);
-//        $this->assertEquals($personCount, $this->em->getRepository(Person::class)->count(array()));
-//        $this->assertEquals('LastName 1', $person->getLastName());
-//    }
-//
-//    public function testGetPersonMultiple() {
-//        $person = new Person();
-//        $person->setLastName('LastName 1');
-//        $person->setFirstName('FirstName 1');
-//        $this->em->persist($person);
-//        $this->em->flush();
-//
-//        $f1 = new EstcMarc();
-//        $f1->setField('100')->setSubfield('a')->setFieldData('LastName 1, FirstName 1.');
-//        $personCount = $this->em->getRepository(Person::class)->count(array());
-//        $this->em->clear();
-//
-//        $found = $this->importer->getPerson(array('100a' => $f1));
-//        $this->assertInstanceOf(Person::class, $found);
-//        $this->assertEquals($personCount, $this->em->getRepository(Person::class)->count(array()));
-//        $this->assertEquals('LastName 1', $found->getLastName());
-//        $this->assertCount(1, $this->importer->getMessages());
-//    }
-//
-//    public function testGetNewPerson() {
-//        $f1 = new EstcMarc();
-//        $f1->setField('100')->setSubfield('a')->setFieldData('Doolittle, Eliza');
-//        $f2 = new EstcMarc();
-//        $f2->setField('100d')->setSubfield('d')->setFieldData('1701-1761.');
-//        $personCount = $this->em->getRepository(Person::class)->count(array());
-//        $this->em->clear();
-//
-//        $person = $this->importer->getPerson(array('100a' => $f1, '100d' => $f2));
-//        $this->assertInstanceOf(Person::class, $person);
-//        $this->assertEquals($personCount + 1, $this->em->getRepository(Person::class)->count(array()));
-//        $this->assertEquals('Doolittle', $person->getLastName());
-//    }
-//
-//    public function testAddAuthor() {
-//        $title = new Title();
-//        $person = new Person();
-//        $this->importer->addAuthor($title, $person);
-//        $this->assertCount(1, $title->getTitleRoles());
-//        $roles = $title->getTitleRoles();
-//        $this->assertNotNull($roles[0]);
-//        $this->assertEquals('Author', $roles[0]->getRole()->getName());
-//    }
+    public function testGetFields() {
+        $f1 = new EstcMarc();
+        $f1->setTitleId('abc')->setField('100')->setSubfield('a')->setFieldData('Bond, James');
+        $f2 = new EstcMarc();
+        $f2->setTitleId('abc')->setField('100')->setSubfield('b')->setFieldData('Moneypenny, Miss');
+
+        $repo = $this->createMock(EstcMarcRepository::class);
+        $repo->method('findBy')->willReturn(array($f1, $f2));
+        $this->importer->setEstcRepo($repo);
+
+        $data = $this->importer->getFields('foo');
+        $this->assertCount(2, $data);
+        $this->assertEquals(array('100a', '100b'), array_keys($data));
+        $this->assertEquals('Bond, James', $data['100a']->getFieldData());
+        $this->assertEquals('Moneypenny, Miss', $data['100b']->getFieldData());
+    }
+
+    public function testCheckTitle() {
+        $repo = $this->createMock(TitleRepository::class);
+        $repo->method('findBy')->willReturn(array('a', 'b'));
+        $this->importer->setTitleRepo($repo);
+
+        $this->assertEquals(2, $this->importer->checkTitle('fooo'));
+        $this->assertCount(1, $this->importer->getMessages());
+    }
+
+    /**
+     * @dataProvider getDatesData
+     */
+    public function testGetDates($expectedDob, $expectedDod, $data) {
+        $f2 = new EstcMarc();
+        $f2->setField('100')->setSubfield('d')->setFieldData($data);
+        list($dob, $dod) = $this->importer->getDates(array('100d' => $f2));
+        $this->assertEquals($expectedDob, $dob);
+        $this->assertEquals($expectedDod, $dod);
+    }
+
+    public function getDatesData() {
+        return array(
+            ['1751', '1801', '1751-1801'],
+            ['1751', '1801', '1751-1801.'],
+            ['1751', '1801', '1751-1801,'],
+            [null, null, null],
+
+            // this is actual data. sigh.
+            ['1698', '1709', 'active 1698-1709.'],
+            ['1468', '1522', '1468?-1522.'],
+            [null, '1714', '-1714]'],
+            [null, '1147', '-1147?.'],
+            [null, 1716, '-1716.'],
+            ['1770', '1820', '1770?-1820?.'],
+            ['1770', null, '1770?-.'],
+            [null, '1520', '-1520 or 1521.'],
+            [null, '1783', '-1783 November 17.'],
+            [null, null, '-approximately 1676.'],
+            ['1752', '1820', '. 1752-1820.'],
+            ['1763', null, '.b. 1763.'],
+            [null, null, '100 B.C.-44 B.C.'],
+            ['1142', '1165', '1141 or 1142-1165.'],
+            [null, null, '121-180.'],
+            ['1388', '', '1388?.-'],
+            ['1580', '1653', '1580-1653)]'],
+            ['1590', null, '1590-approximately 1645.'],
+            ['1603', null, '1602 or 1603-'],
+            ['1605', null, '1605-'],
+        );
+    }
+
+    public function testGetPerson() {
+        $f1 = new EstcMarc();
+        $f1->setField('100')->setSubfield('a')->setFieldData('LastName 1, FirstName 1.');
+        $f2 = new EstcMarc();
+        $f2->setField('100d')->setSubfield('d')->setFieldData('1751-1801.');
+
+        $personCount = $this->em->getRepository(Person::class)->count(array());
+        $this->em->clear();
+
+        $person = $this->importer->getPerson(array('100a' => $f1, '100d' => $f2));
+        $this->assertInstanceOf(Person::class, $person);
+        $this->assertEquals($personCount, $this->em->getRepository(Person::class)->count(array()));
+        $this->assertCount(0, $this->importer->getMessages());
+        $this->assertEquals('LastName 1', $person->getLastName());
+    }
+
+    public function testGetPersonNoDates() {
+        $f1 = new EstcMarc();
+        $f1->setField('100')->setSubfield('a')->setFieldData('LastName 1, FirstName 1.');
+        $personCount = $this->em->getRepository(Person::class)->count(array());
+        $this->em->clear();
+
+        $person = $this->importer->getPerson(array('100a' => $f1));
+        $this->assertInstanceOf(Person::class, $person);
+        $this->assertEquals($personCount, $this->em->getRepository(Person::class)->count(array()));
+        $this->assertEquals('LastName 1', $person->getLastName());
+    }
+
+    public function testGetPersonMultiple() {
+        $person = new Person();
+        $person->setLastName('LastName 1');
+        $person->setFirstName('FirstName 1');
+        $this->em->persist($person);
+        $this->em->flush();
+
+        $f1 = new EstcMarc();
+        $f1->setField('100')->setSubfield('a')->setFieldData('LastName 1, FirstName 1.');
+        $personCount = $this->em->getRepository(Person::class)->count(array());
+        $this->em->clear();
+
+        $found = $this->importer->getPerson(array('100a' => $f1));
+        $this->assertInstanceOf(Person::class, $found);
+        $this->assertEquals($personCount, $this->em->getRepository(Person::class)->count(array()));
+        $this->assertEquals('LastName 1', $found->getLastName());
+        $this->assertCount(1, $this->importer->getMessages());
+    }
+
+    public function testGetNewPerson() {
+        $f1 = new EstcMarc();
+        $f1->setField('100')->setSubfield('a')->setFieldData('Doolittle, Eliza');
+        $f2 = new EstcMarc();
+        $f2->setField('100d')->setSubfield('d')->setFieldData('1701-1761.');
+        $personCount = $this->em->getRepository(Person::class)->count(array());
+        $this->em->clear();
+
+        $person = $this->importer->getPerson(array('100a' => $f1, '100d' => $f2));
+        $this->assertInstanceOf(Person::class, $person);
+        $this->assertEquals($personCount + 1, $this->em->getRepository(Person::class)->count(array()));
+        $this->assertEquals('Doolittle', $person->getLastName());
+    }
+
+    public function testAddAuthor() {
+        $title = new Title();
+        $person = new Person();
+        $this->importer->addAuthor($title, $person);
+        $this->assertCount(1, $title->getTitleRoles());
+        $roles = $title->getTitleRoles();
+        $this->assertNotNull($roles[0]);
+        $this->assertEquals('Author', $roles[0]->getRole()->getName());
+    }
 
     /**
      * @dataProvider guessFormatData
