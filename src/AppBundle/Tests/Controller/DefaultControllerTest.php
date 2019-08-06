d70283bce534108c9ecf710cf35de9ce5b2bd0ec
Michael Joyce <ubermichael@gmail.com>
2019-08-06 12:41:07 -0700

    fix a failing test.

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -2,10 +2,19 @@
 
 namespace AppBundle\Tests\AppBundle\Controller;
 
+use Nines\BlogBundle\DataFixtures\ORM\LoadPage;
+use Nines\UserBundle\DataFixtures\ORM\LoadUser;
 use Nines\UtilBundle\Tests\Util\BaseTestCase;
 
 class DefaultControllerTest extends BaseTestCase
 {
+    protected function getFixtures() {
+        return [
+            LoadPage::class,
+            LoadUser::class,
+        ];
+    }
+
     public function testIndex()
     {
         $client = static::createClient();
