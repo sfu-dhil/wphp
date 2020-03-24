66f7a1e6138bca4c70c9c47e09d74d7a65716147
Michael Joyce <ubermichael@gmail.com>
2020-03-24 15:07:53 -0700

    Make the spotlights dynamic.
    Only the most recent spotlight from each spotlight category will
    appear on the home page in this order: People, Firms, Titles. The home
    page will only display the title and excerpt. Users will need to
    follow the link on the title to get to the full post.
    
    fixes sfu-dhil/wphp#104

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -13,7 +13,9 @@ namespace App\Controller;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Bundle\PaginatorBundle\Definition\PaginatorAwareInterface;
 use Nines\BlogBundle\Entity\Page;
+use Nines\BlogBundle\Entity\Post;
 use Nines\UtilBundle\Controller\PaginatorTrait;
+use Proxies\__CG__\Nines\BlogBundle\Entity\PostCategory;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -34,10 +36,29 @@ class DefaultController extends AbstractController implements PaginatorAwareInte
      * @return array
      */
     public function indexAction(EntityManagerInterface $em) {
-        $repo = $em->getRepository(Page::class);
-        $page = $repo->findHomepage();
+        $pageRepo = $em->getRepository(Page::class);
+
+        $spotlightCategories = [
+            $em->find(PostCategory::class, 1),
+            $em->find(PostCategory::class, 2),
+            $em->find(PostCategory::class, 3),
+        ];
+        $qb = $em->createQueryBuilder();
+        $qb->select('p');
+        $qb->from(Post::class, 'p');
+        $qb->where('p.category = :category');
+        $qb->innerJoin('p.status', 's');
+        $qb->andWhere('s.public = 1');
+        $qb->orderBy('p.id', 'desc');
+
+        $spotlights = [];
+        foreach($spotlightCategories as $cat) {
+            $spotlights[] = $qb->getQuery()->setParameter('category', $cat)->getOneOrNullResult();
+        }
+
         return [
-            'homepage' => $page,
+            'homepage' => $pageRepo->findHomepage(),
+            'spotlights' => $spotlights,
         ];
     }
 
