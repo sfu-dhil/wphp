0ef92591e589c30389230b0a3d6f80afea3e28dc
Joey Takeda <joey.takeda@gmail.com>
2021-09-08 18:03:26 -0700

    Restoring editor config

--- public/js/editor-config.js
+++ public/js/editor-config.js
@@ -1,4 +1,5 @@
 function getTinyMceConfig(editorUploadPath) {
+
     return {
         branding: false,
         selector: '.tinymce',
@@ -11,7 +12,7 @@ function getTinyMceConfig(editorUploadPath) {
 
         toolbar: [
             'undo redo | styleselect | pastetext | bold italic | alignleft aligncenter alignright alignjustify | table',
-            'bullist numlist | outdent indent | link | charmap | code | bibliography'],
+            'bullist numlist | outdent indent | link | charmap | code'],
 
         browser_spellcheck: true,
 
@@ -24,29 +25,7 @@ function getTinyMceConfig(editorUploadPath) {
         resize: true,
         paste_as_text: true,
         paste_block_drop: true,
-        setup: function (editor) {
 
-            editor.ui.registry.addButton('bibliography', {
-                icon: 'checklist',
-                tooltip: 'Convert to bibliography',
-                disabled: true,
-                onAction: function (_) {
-                    editor.focus();
-                    editor.selection.setContent('<div class="bibliography" style="background:red;">' + editor.selection.getContent() + "</div>");
-                },
-                onSetup: function (buttonApi) {
-                    var editorEventCallback = function (eventApi) {
-                        buttonApi.setDisabled(eventApi.element.nodeName.toLowerCase() === 'time');
-                    };
-                    editor.on('NodeChange', editorEventCallback);
-
-                    /* onSetup should always return the unbind handlers */
-                    return function (buttonApi) {
-                        editor.off('NodeChange', editorEventCallback);
-                    };
-                }
-            });
-        },
         style_formats_merge: true,
         style_formats: [{
                 title: 'Image Left',
