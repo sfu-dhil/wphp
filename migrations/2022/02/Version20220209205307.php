fb8121bb5dce77c71ebd3878d141f1749f6492b7
Michael Joyce <ubermichael@gmail.com>
2022-04-28 11:20:05 -0700

    update part 1, something running

---
+++ migrations/2022/02/Version20220209205307.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * (c) 2021 Michael Joyce <mjoyce@sfu.ca>
+ * This source file is subject to the GPL v2, bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220209205307 extends AbstractMigration {
+    public function getDescription() : string {
+        return '';
+    }
+
+    public function up(Schema $schema) : void {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('RENAME TABLE blog_page TO nines_blog_page');
+        $this->addSql('RENAME TABLE blog_post TO nines_blog_post');
+        $this->addSql('RENAME TABLE blog_post_category TO nines_blog_post_category');
+        $this->addSql('RENAME TABLE blog_post_status TO nines_blog_post_status');
+    }
+
+    public function down(Schema $schema) : void {
+        $this->throwIrreversibleMigrationException();
+    }
+}
