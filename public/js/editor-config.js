0cc4d5d0c8d914960d333b517e56cfc4543bd2a7
Andrew Gardener <andrew.e.gardener@gmail.com>
2025-04-11 15:21:19 -0700

    Add tinymce upload limit
    Limit hard coded to 20MB
    
    Closes #339

--- public/js/editor-config.js
+++ public/js/editor-config.js
@@ -25,6 +25,49 @@ function getTinyMceConfig(editorUploadPath) {
         resize: true,
         paste_as_text: true,
         paste_block_drop: true,
+        images_upload_handler: function (blobInfo, success, failure, progress) {
+            const MAX_SIZE = 20 * 1024 * 1024 // 20 MB
+            // image size in bytes
+            const image_size = blobInfo.blob().size
+            if (image_size > MAX_SIZE) {
+                image_size_in_mb = (image_size * 1.0 / 1024 / 1024).toFixed(2)
+                failure(`The image exceeds the maximum upload size of 20 MB. <br />Image size is: ${image_size_in_mb} MB.`)
+                return
+            }
+            // taken from https://www.tiny.cloud/docs/tinymce/5/upload-images/#_example_using_images_upload_handler
+            const xhr = new XMLHttpRequest()
+            xhr.withCredentials = true
+            xhr.open('POST', editorUploadPath)
+
+            xhr.upload.onprogress = function (e) {
+              progress(e.loaded / e.total * 100)
+            }
+
+            xhr.onload = function() {
+              if (xhr.status === 403) {
+                failure('HTTP Error: ' + xhr.status, { remove: true })
+                return
+              }
+              if (xhr.status < 200 || xhr.status >= 300) {
+                failure('HTTP Error: ' + xhr.status)
+                return
+              }
+              const json = JSON.parse(xhr.responseText)
+              if (!json || typeof json.location != 'string') {
+                failure('Invalid JSON: ' + xhr.responseText)
+                return
+              }
+              success(json.location)
+            }
+
+            xhr.onerror = function () {
+              failure('Image upload failed due to a XHR Transport error. Code: ' + xhr.status)
+            }
+
+            const formData = new FormData()
+            formData.append('file', blobInfo.blob(), blobInfo.filename())
+            xhr.send(formData)
+        },
 
         style_formats_merge: true,
         style_formats: [{
@@ -53,6 +96,5 @@ function getTinyMceConfig(editorUploadPath) {
                 },
             },
         ],
-    };
-
+    }
 }
